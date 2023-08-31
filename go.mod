5706bb4727d04089ee27eb7fbd1c2ac6199e483f
Guy Davies <aguydavies@gmail.com>
2023-08-31 14:36:40 +0200

    change model for providing max for random generator

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/guydavies/private_ipv6_2
 
 go 1.21.0
+
+require github.com/guydavies/hexmanip v0.0.0-20230831120520-60240ddf78e7 // indirect
--- private_ipv6.go
+++ private_ipv6.go
@@ -9,8 +9,9 @@ package main
 
 import (
 	"fmt"
-	"github.com/guydavies/hexmanip"
 	"strings"
+
+	"github.com/guydavies/hexmanip"
 )
 
 func main() {
@@ -21,18 +22,18 @@ func main() {
 	//
 	// Then iterate three times to construct three more random hex blocks of 4
 	// hex digits (two bytes) each
-	var hex_digits_per_group int = 4
+	var max_hex_value_as_int int = 65535
 	var hex_first_byte string = "fd"
 	var hex_two_bytes []string
 	var addr_all_groups []string
 	hex_two_bytes = append(hex_two_bytes, hex_first_byte)
-	var hex_byte string = hexmanip.Generate_hex(2)
+	var hex_byte string = hexmanip.Generate_hex(15)
 	hex_two_bytes = append(hex_two_bytes, hex_byte)
 	var hex_two_byte_str string = strings.Join(hex_two_bytes, "")
 	addr_all_groups = append(addr_all_groups, hex_two_byte_str)
 
 	for i := 0; i < 3; i++ {
-		addr_all_groups = append(addr_all_groups, hexmanip.Generate_hex(hex_digits_per_group))
+		addr_all_groups = append(addr_all_groups, hexmanip.Generate_hex(max_hex_value_as_int))
 	}
 
 	addr_all_groups = append(addr_all_groups, ":/64")
